7675534befdc47941c5a3a16ff890e330d974df1
1s22s1 <1s22s1@@users.noreply.github.com>
2023-01-15 20:46:56 +0900

    Add eslint

--- gcd.js
+++ gcd.js
@@ -1,18 +1,16 @@
-function gcd(m, n) {
-    let [a, b] = m > n ? [m, n] : [n, m];
+function gcd (m, n) {
+  let [a, b] = m > n ? [m, n] : [n, m]
 
-    while(true) {
-        const quotient = Math.floor(a / b);
-        const remainder = a % b;
+  while (true) {
+    const remainder = a % b
 
-        if (remainder == 0) {
-            return b;
-        }
-
-        a = b;
-        b = remainder;
+    if (remainder === 0) {
+      return b
     }
 
+    a = b
+    b = remainder
+  }
 }
 
-module.exports = gcd;
+module.exports = gcd
