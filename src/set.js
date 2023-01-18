8e9744678f85997dfef3299f5e8142ccb435458d
1s22s1 <1s22s1@@users.noreply.github.com>
2023-01-19 03:04:20 +0900

    Define difference set

--- src/set.js
+++ src/set.js
@@ -9,3 +9,9 @@ exports.intersection = (arr1, arr2) => {
 
   return [...new Set(arr)].sort((m, n) => m - n)
 }
+
+exports.difference = (arr1, arr2) => {
+  const arr = arr1.filter(e => !arr2.includes(e))
+
+  return [...new Set(arr)].sort((m, n) => m - n)
+}
--- test/set.test.js
+++ test/set.test.js
@@ -27,3 +27,17 @@ describe('intersection', () => {
     expect(set.intersection([1, 2], [])).toEqual([])
   })
 })
+
+describe('difference', () => {
+  test('given 1, 2 and 2, 4 is 1, 2, 4', () => {
+    expect(set.difference([1, 2], [2, 4])).toEqual([1])
+  })
+
+  test('given 1, 2 and 1, 2 is 1, 2', () => {
+    expect(set.difference([1, 2], [1, 2])).toEqual([])
+  })
+
+  test('given 1, 2 and the empty set is 1, 2', () => {
+    expect(set.difference([1, 2], [])).toEqual([1, 2])
+  })
+})
