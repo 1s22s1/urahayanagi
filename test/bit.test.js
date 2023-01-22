d2e71fd3eb6010369e6089157e41d2a30a66f387
1s22s1 <1s22s1@@users.noreply.github.com>
2023-01-22 18:01:35 +0900

    Use each

--- test/bit.test.js
+++ test/bit.test.js
@@ -1,19 +1,12 @@
 const bit = require('../src/bit')
 
-describe('isOn', () => {
-  test('The 0th bit of 10 is off', () => {
-    expect(bit.isOn(10, 0)).toBe(false)
-  })
-
-  test('The 1st bit of 10 is on', () => {
-    expect(bit.isOn(10, 1)).toBe(true)
-  })
-
-  test('The 2nd bit of 10 is off', () => {
-    expect(bit.isOn(10, 2)).toBe(false)
-  })
-
-  test('The 3rd bit of 10 is on', () => {
-    expect(bit.isOn(10, 3)).toBe(true)
+describe('.isOn', () => {
+  test.each([
+    [0, 10, false],
+    [1, 10, true],
+    [2, 10, false],
+    [3, 10, true]
+  ])('The %dth bit %d is %s', (bitNum, num, expected) => {
+    expect(bit.isOn(num, bitNum)).toBe(expected)
   })
 })
